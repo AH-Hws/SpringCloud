52998acc2e0d00f16efc83067b999e59cbec8681
wilcom <1>
2022-09-03 18:37:34 +0800

    1

--- EurakeServer/src/main/java/com/discovery/server/DiscoveryApp.java
+++ EurakeServer/src/main/java/com/discovery/server/DiscoveryApp.java
@@ -1,28 +1,15 @@
 package com.discovery.server;
 
-import com.discovery.server.dao.UserMapper;
-import com.discovery.server.dao.UserMapperImpl2;
-import com.discovery.server.service.UserService;
-import com.discovery.server.service.UserServiceImp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author Huws
  */
-//@EnableEurekaServer
-
-@ComponentScan
+@SpringBootApplication
 public class DiscoveryApp {
 
     public static void main(String[] args) {
-//        ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(DiscoveryApp.class, args);
-//        UserService userService = (UserService) configurableApplicationContext.getBean(UserServiceImp.class);
-//        System.out.println(userService);
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(DiscoveryApp.class);
-        System.out.println(applicationContext.getBean(UserMapper.class, "1121"));
+       SpringApplication.run(DiscoveryApp.class, args);
     }
 }
